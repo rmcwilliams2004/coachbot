d1cd7a95b80a1b187bf21f69b5d9b973458d1075
Stephen Starkey <stephen@centriqhome.com>
2016-11-01 16:34:44 -0500

    Upgraded clj-http

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
 
 (defproject coachbot "0.1.0-SNAPSHOT"
   :description "Simple, elegant, automatic motivation"
-  :dependencies [[clj-http "2.3.0"]
+  :dependencies [[clj-http "3.3.0"]
                  [com.taoensso/timbre "4.7.4"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/data.codec "0.1.0"]
