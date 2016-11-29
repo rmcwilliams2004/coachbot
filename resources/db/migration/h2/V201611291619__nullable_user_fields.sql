13f62cf97befa42bb9172aa15f1092c27ef0c441
Stephen Starkey <stephen@centriqhome.com>
2016-11-29 16:21:38 -0600

    Nullable fields.

---
+++ resources/db/migration/h2/V201611291619__nullable_user_fields.sql
@@ -0,0 +1,5 @@
+ALTER TABLE slack_coaching_users
+  ALTER first_name SET NULL;
+
+ALTER TABLE slack_coaching_users
+  ALTER last_name SET NULL;
---
+++ resources/db/migration/mysql/V201611291619__nullable_user_fields.sql
@@ -0,0 +1,3 @@
+ALTER TABLE slack_coaching_users MODIFY COLUMN first_name VARCHAR(128);
+
+ALTER TABLE slack_coaching_users MODIFY COLUMN last_name VARCHAR(128);
