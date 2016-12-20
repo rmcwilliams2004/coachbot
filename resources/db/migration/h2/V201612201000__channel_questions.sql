1ddde7447543cb9df5fbe286db48b9ac18e637a6
Stephen Starkey <stephen@centriqhome.com>
2016-12-20 11:36:21 -0600

    Filled out foreign key constraints for diagramming purposes.

--- resources/db/migration/h2/V201612201000__channel_questions.sql
+++ resources/db/migration/h2/V201612201000__channel_questions.sql
@@ -1,31 +1,40 @@
 CREATE TABLE slack_coaching_channels (
   id           MEDIUMINT NOT NULL         AUTO_INCREMENT,
-
+  team_id      MEDIUMINT NOT NULL,
+  channel_id   VARCHAR(64),
   created_date TIMESTAMP                  DEFAULT CURRENT_TIMESTAMP,
   updated_date TIMESTAMP AS current_timestamp(),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  UNIQUE (team_id, channel_id)
 );
 
 CREATE TABLE channel_questions (
-  id           MEDIUMINT NOT NULL         AUTO_INCREMENT,
-
-  created_date TIMESTAMP                  DEFAULT CURRENT_TIMESTAMP,
-  updated_date TIMESTAMP AS current_timestamp(),
+  id              MEDIUMINT NOT NULL         AUTO_INCREMENT,
+  channel_id      MEDIUMINT NOT NULL,
+  question        VARCHAR(255),
+  left_scale_lbl  VARCHAR(64),
+  right_scale_lbl VARCHAR(64),
+  created_date    TIMESTAMP                  DEFAULT CURRENT_TIMESTAMP,
+  updated_date    TIMESTAMP AS current_timestamp(),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE channel_questions_asked (
-  id           MEDIUMINT NOT NULL         AUTO_INCREMENT,
-
-  created_date TIMESTAMP                  DEFAULT CURRENT_TIMESTAMP,
-  updated_date TIMESTAMP AS current_timestamp(),
+  id                   MEDIUMINT NOT NULL         AUTO_INCREMENT,
+  question_id          MEDIUMINT NOT NULL,
+  expiration_timestamp TIMESTAMP NOT NULL,
+  created_date         TIMESTAMP                  DEFAULT CURRENT_TIMESTAMP,
+  updated_date         TIMESTAMP AS current_timestamp(),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE channel_questions_answered (
   id           MEDIUMINT NOT NULL         AUTO_INCREMENT,
-
+  scu_id       MEDIUMINT NOT NULL,
+  qa_id        MEDIUMINT NOT NULL,
+  answer       TINYINT,
   created_date TIMESTAMP                  DEFAULT CURRENT_TIMESTAMP,
   updated_date TIMESTAMP AS current_timestamp(),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  UNIQUE (scu_id, qa_id)
 );
