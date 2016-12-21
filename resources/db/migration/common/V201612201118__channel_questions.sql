2aea68343bbfe041f1b55124bc7491ee4a7bd667
Stephen Starkey <stephen@centriqhome.com>
2016-12-21 14:07:07 -0600

    Fixed constraint table reference.

--- resources/db/migration/common/V201612201118__channel_questions.sql
+++ resources/db/migration/common/V201612201118__channel_questions.sql
@@ -10,7 +10,7 @@ ALTER TABLE channel_questions_asked
   ADD CONSTRAINT fk_cqasked_cq FOREIGN KEY (question_id)
 REFERENCES channel_questions (id);
 
-ALTER TABLE channel_questions_answered
+ALTER TABLE channel_question_answers
   ADD CONSTRAINT fk_cqa_cqasked FOREIGN KEY (qa_id)
 REFERENCES channel_questions_asked (id);
 
